0db60f867dc7291fffa6fbbd442b66ee47948883
RobertRish <robert.rish1225@gmail.com>
2022-08-27 09:00:21 -0400

    git user test

--- game.js
+++ game.js
@@ -3,3 +3,4 @@ const chioces = document.querySelectorAll('#.chioce-text');
 const question = document.querySelector('#question');
 const question = document.querySelector('#question');
 const question = document.querySelector('#question');
+/*git user test*/
