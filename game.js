b96dc4e194fb1f12640622a2499544d1789bf276
RobertRish <robert.rish1225@gmail.com>
2022-09-03 18:11:22 -0400

    Finished scripting game.js and added new files for end screen

--- game.js
+++ game.js
@@ -1,11 +1,111 @@
 const question = document.querySelector('#question');
-const chioces = document.querySelectorAll('#.chioce-text');
-const question = document.querySelector('#question');
-const question = document.querySelector('#question');
-const question = document.querySelector('#question');
-/*git user test*/
-/*git user test*/
-/*git user test 4*/
-/*git user test 3*/
-/*git user test 5*/
-/*git user test 6*/
+const chioces = Array.from(document.querySelectorAll('#.chioce-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText= document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0;
+let availableQuestions = []
+
+let questions = [
+    {
+        question: 'What is ?',
+        choice1: '1',
+        choice2: '2',
+        choice3: '3',
+        choice4: 'a;owefij',
+        answer: 2
+    },
+    {
+        question: 'What is it?',
+        choice1: '1',
+        choice2: '2e',
+        choice3: '3',
+        choice4: 'a;owefij',
+        answer: 2
+    },
+    {
+        question: 'What is bulg?',
+        choice1: '1',
+        choice2: '2',
+        choice3: '3',
+        choice4: 'a;owefij',
+        answer: 2
+    },
+    {
+        question: 'What is fun ?',
+        choice1: '1',
+        choice2: '2',
+        choice3: '3',
+        choice4: 'a;owefij',
+        answer: 2
+    }
+]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestions()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%` 
+
+    const questionsIndex = MAth.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionIndex]
+    question.innerText = currentQuestion.question
+
+    chioces.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+chioces.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.targetconst 
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        } 
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+        }, 1000)
+    })
+})
+
+incrementScore = num => { 
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame()
